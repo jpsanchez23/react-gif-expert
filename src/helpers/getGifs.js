3258a4e811cecc9084328f5bcea1543f4b17a678
Juan Sánchez <sanchezfjuanp@gmail.com>
2023-12-18 09:55:45 -0300

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=4X1t42g6IUMf7j5eEnxi0HuDPgzsOn12&limit=20&q=${ category }`;
+
+    const resp = await fetch( url );
+    
+    const { data = [] } = await resp.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    console.log( gifs );
+    return gifs;
+}
